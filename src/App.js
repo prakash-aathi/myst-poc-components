966855d085920498d85ebf6171746acaba4cb96b
Prakash A <prakashaathi321@gmail.com>
2024-08-23 22:28:08 +0530

    chore: Add react-icons and react-table-lite dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MyDataTable from "./components/table/MyDataTable";
 
 function App() {
+  const handleEdit = (args, event, row) => {
+    alert(`Edit Row: ${JSON.stringify(row)}`);
+  };
+
+  const handleDelete = (args, event, row) => {
+    alert(`Delete Row: ${JSON.stringify(row)}`);
+  };
+
+  const handleView = (args, event, row) => {
+    alert(`View Row: ${JSON.stringify(row)}`);
+  };
+
+  const customButtons = {
+    view: (row) => (
+      <button
+        onClick={(event) => handleView("other-variables", event, row)}
+        style={{ background: "blue", color: "white", margin: "0 2px" }}
+      >
+        View
+      </button>
+    ),
+    edit: (row) => (
+      <button
+        onClick={(event) => handleEdit("other-variables", event, row)}
+        style={{ background: "green", color: "white", margin: "0 2px" }}
+      >
+        Edit
+      </button>
+    ),
+    delete: (row) => (
+      <button
+        onClick={(event) => handleDelete("other-variables", event, row)}
+        style={{ background: "red", color: "white", margin: "0 2px" }}
+      >
+        Delete
+      </button>
+    ),
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyDataTable
+      headers={["id", "part_no", "part_qty", "price"]}
+      sortBy={["part_no", "part_qty", "price"]}
+      showActions={true}
+      actionTypes={["edit", "delete", "view"]}
+      customRenderActions={customButtons}
+      customHeaders={{
+        id: "ID",
+        part_no: "Part Number",
+        part_qty: "Quantity",
+        price: "Price ($)",
+      }}
+    />
   );
 }
 
